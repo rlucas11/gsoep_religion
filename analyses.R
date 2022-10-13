ed31ce301c5e2eb8004ac2dc96f3f72650126b09
rich <rlucas11@yahoo.com>
2022-10-13 06:12:18 -0400

    Updated script for extracting results from riclpm

--- analyses.R
+++ analyses.R
@@ -41,6 +41,7 @@ sink()
 
 ## Temporarily save model as file
 save(model1, file = "info/model1.RData")
+load("info/model1.RData")
 
 ## List estimate labels for estimate-extraction function
 estimateLabels <- data.frame(matrix(c(
@@ -137,7 +138,7 @@ riclpmStandardizedResults <- standardizedSolution(modelRiclpm,
     type = "std.all", se = TRUE, zstat = TRUE,
     pvalue = TRUE, ci = TRUE, level = .95, output = "text"
 )
-clpmStandardizedResults
+riclpmStandardizedResults
 fitMeasures(modelRiclpm)
 sink()
 
@@ -145,6 +146,101 @@ clpmStandardizedResults
 
 ## Temporarily save model.extract
 save(modelRiclpm, file = "info/riclpmModel.RData")
+load("info/riclpmModel.RData")
+
+## List estimate labels for estimate-extraction function
+riclpmEstimateLabels <- data.frame(matrix(c(
+    "agr", "c_ra", "c_ar", "sa", "r1_ra", "r2_ra", "ri_ar",
+    "cns", "c_rc", "c_cr", "sc", "r1_rc", "r2_rc", "ri_cr",
+    "ext", "c_re", "c_er", "se", "r1_re", "r2_re", "ri_er",
+    "neu", "c_rn", "c_nr", "sn", "r1_rn", "r2_rn", "ri_nr",
+    "opn", "c_ro", "c_or", "so", "r1_ro", "r2_ro", "ri_or"
+),
+nrow = 5,
+ncol = 7,
+byrow = TRUE
+))
+
+names(riclpmEstimateLabels) <- c("trait", "cl_rOnt", "cl_tOnr", "stability", "r1", "r2", "ri_r")
+
+
+## Function to extract standardized estimates for meta-analysis
+## Extracts estimate of r, along with 95% CIs
+## Function works on one trait at a time (e.g., a line of estimateLabels)
+
+riclpmExtractEstimates <- function(resultsLabels, results) {
+    riclpmResults <- data.frame(
+        trait = character(),
+        r1 = numeric(),
+        r1.lb = numeric(),
+        r1.ub = numeric(),
+        r2 = numeric(),
+        r2.lb = numeric(),
+        r2.ub = numeric(),
+        rt.cl = numeric(),
+        rt.cl.lb = numeric(),
+        rt.cl.ub = numeric(),
+        tr.cl = numeric(),
+        tr.cl.lb = numeric(),
+        tr.cl.ub = numeric(),
+        st = numeric(),
+        st.lb = numeric(),
+        st.ub = numeric(),
+        ri_r = numeric(),
+        ri_r.lb = numeric(),
+        ri_r.ub = numeric()
+    )
+    riclpmResults[1, "trait"] <- resultsLabels$trait
+    riclpmResults[1, "r1"] <- results[results$label == resultsLabels[[1, "r1"]], "est.std"]
+    riclpmResults[1, "r1.lb"] <- results[results$label == resultsLabels[[1, "r1"]], "ci.lower"]
+    riclpmResults[1, "r1.ub"] <- results[results$label == resultsLabels[[1, "r1"]], "ci.upper"]
+    riclpmResults[1, "r2"] <- mean(results[results$label == resultsLabels[[1, "r2"]], "est.std"])
+    riclpmResults[1, "r2.lb"] <- mean(results[results$label == resultsLabels[[1, "r2"]], "ci.lower"])
+    riclpmResults[1, "r2.ub"] <- mean(results[results$label == resultsLabels[[1, "r2"]], "ci.upper"])
+    riclpmResults[1, "rt.cl"] <- mean(results[results$label == resultsLabels[[1, "cl_rOnt"]], "est.std"])
+    riclpmResults[1, "rt.cl.lb"] <- mean(results[results$label == resultsLabels[[1, "cl_rOnt"]], "ci.lower"])
+    riclpmResults[1, "rt.cl.ub"] <- mean(results[results$label == resultsLabels[[1, "cl_rOnt"]], "ci.upper"])
+    riclpmResults[1, "tr.cl"] <- mean(results[results$label == resultsLabels[[1, "cl_tOnr"]], "est.std"])
+    riclpmResults[1, "tr.cl.lb"] <- mean(results[results$label == resultsLabels[[1, "cl_tOnr"]], "ci.lower"])
+    riclpmResults[1, "tr.cl.ub"] <- mean(results[results$label == resultsLabels[[1, "cl_tOnr"]], "ci.upper"])
+    riclpmResults[1, "st"] <- mean(results[results$label == resultsLabels[[1, "stability"]], "est.std"])
+    riclpmResults[1, "st.lb"] <- mean(results[results$label == resultsLabels[[1, "stability"]], "ci.lower"])
+    riclpmResults[1, "st.ub"] <- mean(results[results$label == resultsLabels[[1, "stability"]], "ci.upper"])
+    riclpmResults[1, "ri_r"] <- mean(results[results$label == resultsLabels[[1, "ri_r"]], "est.std"])
+    riclpmResults[1, "ri_r.lb"] <- mean(results[results$label == resultsLabels[[1, "ri_r"]], "ci.lower"])
+    riclpmResults[1, "ri_r.ub"] <- mean(results[results$label == resultsLabels[[1, "ri_r"]], "ci.upper"])
+    return(riclpmResults)
+}
+
+## Extract all the estimates in a loop
+rm(combinedRiclpmResults)
+combinedRiclpmResults <- data.frame(
+    trait = character(),
+    r1 = numeric(),
+    r1.lb = numeric(),
+    r1.ub = numeric(),
+    r2 = numeric(),
+    r2.lb = numeric(),
+    r2.ub = numeric(),
+    rt.cl = numeric(),
+    rt.cl.lb = numeric(),
+    rt.cl.ub = numeric(),
+    tr.cl = numeric(),
+    tr.cl.lb = numeric(),
+    tr.cl.ub = numeric(),
+    st = numeric(),
+    st.lb = numeric(),
+    st.ub = numeric(),
+    ri_r = numeric(),
+    ri_r.lb = numeric(),
+    ri_r.ub = numeric()
+)
+for (i in 1:nrow(riclpmEstimateLabels)) {
+    tempResults <- riclpmExtractEstimates(riclpmEstimateLabels[i, ], riclpmStandardizedResults)
+    combinedRiclpmResults[i, ] <- tempResults
+}
+combinedRiclpmResults
+
 
 ################################################################################
 ## All Traits, Observed Variables, full sample
@@ -157,7 +253,6 @@ model.all.observed <- sem(riclpm_observed,
 )
 summary(model.all.observed)
 fitMeasures(model.all.observed)
-
 standardizedSolution(model.all.observed,
                      type = "std.all", se = TRUE, zstat = TRUE,
                      pvalue = TRUE, ci = TRUE, level = .95, output = "text"
