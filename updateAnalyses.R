5f5c17d56cf3ddf42c0de327b29b1f19b18f4704
rich <rlucas11@yahoo.com>
2022-10-25 08:09:13 -0400

    Changed 'updateAnalyses.R' to run fixed single-trait code

--- updateAnalyses.R
+++ updateAnalyses.R
@@ -13,20 +13,20 @@ source('restrictSample.R')
 ## data$first.state <- sample(1:50, nrow(data), replace=TRUE)
 ## bula_neu <- sort(unique(data$first.state))[1:3]
 
-## Get correlations
-source('correlations.R')
+## ## Get correlations
+## source('correlations.R')
 
-## Run original model (all traits, latent, state-by-state)
-source('clpm.latent.all.R')
+## ## Run original model (all traits, latent, state-by-state)
+## source('clpm.latent.all.R')
 
-## Run RI-CLPM equivalent of original (all traits, latent, state-by-state)
-source('riclpm.latent.all.R')
+## ## Run RI-CLPM equivalent of original (all traits, latent, state-by-state)
+## source('riclpm.latent.all.R')
 
-## Run original model with observed traits (all traits, observed, state-by-state)
-source('clpm.observed.all.R')
+## ## Run original model with observed traits (all traits, observed, state-by-state)
+## source('clpm.observed.all.R')
 
-## Run RI-CLPM with observed traits (all traits, observed, state-by-state)
-source('riclpm.observed.all.R')
+## ## Run RI-CLPM with observed traits (all traits, observed, state-by-state)
+## source('riclpm.observed.all.R')
 
 ## Run CLPM with latent variables, trait-by-trait (trait-by-trait, latent, state-by-state)
 source('clpm.single.R')
