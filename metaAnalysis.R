6f5c69d00dbd02b9f2579b8a2af70474fed1cfe6
rich <rlucas11@yahoo.com>
2022-10-25 08:08:00 -0400

    Fixed error with single-trait models (wrong names)

--- metaAnalysis.R
+++ metaAnalysis.R
@@ -3,13 +3,29 @@ library(tidyverse)
 
 data <- read_csv("results/clpm.latent.states.aggregated.estimates.csv")
 
-yivi <- escalc(measure="COR", ri=opn.rt.cl, ni=samplesize, data=data)
+yivi <- escalc(measure="COR", ri=neu.rt.cl, ni=samplesize, data=data)
 meta <- rma(yi, vi, data=yivi, method="ML")
 summary(meta)
 
 
 
 dataRi <- read_csv("results/riclpm.latent.states.aggregated.estimates.csv")
+
+yiviRi <- escalc(measure="COR", ri=neu.rt.cl, ni=samplesize, data=dataRi)
+metaRi <- rma(yi, vi, data=yiviRi, method="ML")
+summary(metaRi)
+
+
+data <- read_csv("results/clpm.observed.states.aggregated.estimates.csv")
+
+yivi <- escalc(measure="COR", ri=opn.rt.cl, ni=samplesize, data=data)
+meta <- rma(yi, vi, data=yivi, method="ML")
+summary(meta)
+
+
+
+dataRi <- read_csv("results/riclpm.observed.states.aggregated.estimates.csv")
+
 yiviRi <- escalc(measure="COR", ri=opn.rt.cl, ni=samplesize, data=dataRi)
 metaRi <- rma(yi, vi, data=yiviRi, method="ML")
 summary(metaRi)
