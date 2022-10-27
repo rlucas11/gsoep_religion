ea3c33d5c938a16558df75fcd57dd9bfcb63005e
rich <rlucas11@yahoo.com>
2022-10-27 19:59:04 -0400

    Continued work on meta-analysis

--- metaAnalysis.R
+++ metaAnalysis.R
@@ -1,5 +1,6 @@
 library(metafor)
 library(tidyverse)
+library(ggplot2)
 
 ################################################################################
 ## Setup
@@ -29,6 +30,154 @@ varNames <- data.frame(
     sampleSize = "samplesize"
 )
 
+bulaList <- data.frame(state = c(1:16),
+                       stateName = c("Schleswig-Holstein",
+                                      "Hamburg",
+                                      "Lower Saxony",
+                                      "Bremen",
+                                      "North Rhine-Westphalia",
+                                      "Hesse",
+                                      "Rhineland-Palatinate",
+                                      "Baden-Wuerttemberg",
+                                      "Bavaria",
+                                      "Saarland",
+                                      "Berlin",
+                                      "Brandenburg",
+                                      "Mecklenburg-Western Pomerania",
+                                      "Saxony",
+                                      "Saxony-Anhalt",
+                                     "Thuringia"),
+                       stringsAsFactors = FALSE)
+bulaList <- left_join(bulaList, religiosity, by="state")
+bulaList <- bulaList %>%
+    arrange(desc(z.relig)) %>%
+    mutate(stateName = factor(stateName, unique(stateName)))
+
+
+
+################################################################################
+## Load Data
+################################################################################
+
+data <- read_csv("results/clpm.latent.states.aggregated.estimates.csv")
+data <- left_join(data, bulaList[, c("state", "stateName", "z.relig")], by = "state")
+
+dataRi <- read_csv("results/riclpm.latent.states.aggregated.estimates.csv")
+dataRi <- left_join(dataRi, bulaList[, c("state", "stateName", "z.relig")], by = "state")
+
+dataObs <- read_csv("results/clpm.observed.states.aggregated.estimates.csv")
+dataObs <- left_join(dataObs, bulaList[, c("state", "stateName", "z.relig")], by = "state")
+
+dataRiObs <- read_csv("results/riclpm.observed.states.aggregated.estimates.csv")
+dataRiObs <- left_join(dataRiObs, bulaList[, c("state", "stateName", "z.relig")], by = "state")
+
+dataSingle <- read_csv("results/clpm.states.single.estimates.csv")
+dataSingle <- left_join(dataSingle, bulaList[, c("state", "stateName", "z.relig")], by = "state")
+
+dataRiSingle <- read_csv("results/riclpm.states.single.estimates.csv")
+dataRiSingle <- left_join(dataRiSingle, bulaList[, c("state", "stateName", "z.relig")], by = "state")
+
+################################################################################
+## Plots
+################################################################################
+
+## Use grep to find errors
+## grep("instabilities|positive\ definite|variances\ are\ negative", clpm.warnings)
+
+## Load results
+cl.rt.models.orig <- data %>%
+    select(starts_with("agr.rt.cl"), state, stateName, z.relig) %>%
+    mutate(Model="CLPM",
+           Type="Latent")
+names(cl.rt.models.orig) <- c("rt.cl", "ub", "lb", "stateId", "State", "Religiosity", "Model", "Type")
+## Load warnings
+load("results/clpm.warnings.RData")
+## Find any with problems
+problems <- as.numeric(grep("instabilities|positive\ definite|variances\ are\ negative", clpm.warnings))
+cl.rt.models.orig[!(cl.rt.models.orig$stateId %in% problems), "select"] <- 1
+
+cl.rt.models.ri <- dataRi %>%
+    select(starts_with("agr.rt.cl"), state, stateName, z.relig) %>%
+    mutate(Model="RI-CLPM",
+           Type="Latent")
+names(cl.rt.models.ri) <- c("rt.cl", "ub", "lb", "stateId", "State", "Religiosity", "Model", "Type")
+## Load warnings
+load("results/riclpm.warnings.RData")
+## Find any with problems
+problems <- as.numeric(grep("instabilities|positive\ definite|variances\ are\ negative", riclpm.warnings))
+cl.rt.models.ri[!(cl.rt.models.ri$stateId %in% problems), "select"] <- 1
+
+
+cl.rt.models.obs <- dataObs %>%
+    select(starts_with("agr.rt.cl"), state, stateName, z.relig) %>%
+    mutate(Model="CLPM",
+           Type="Observed")
+names(cl.rt.models.obs) <- c("rt.cl", "ub", "lb", "stateId", "State", "Religiosity", "Model", "Type")
+## Load warnings
+load("results/clpm.observed.warnings.RData")
+## Find any with problems
+problems <- as.numeric(grep("instabilities|positive\ definite|variances\ are\ negative", clpm.warnings))
+cl.rt.models.obs[!(cl.rt.models.obs$stateId %in% problems), "select"] <- 1
+
+
+cl.rt.models.ri.obs <- dataRiObs %>%
+    select(starts_with("agr.rt.cl"), state, stateName, z.relig) %>%
+    mutate(Model="RI-CLPM",
+           Type="Observed")
+names(cl.rt.models.ri.obs) <- c("rt.cl", "ub", "lb", "stateId", "State", "Religiosity", "Model", "Type")
+## Load warnings
+load("results/riclpm.observed.warnings.RData")
+## Find any with problems
+problems <- as.numeric(grep("instabilities|positive\ definite|variances\ are\ negative", riclpm.warnings))
+cl.rt.models.ri.obs[!(cl.rt.models.ri.obs$stateId %in% problems), "select"] <- 1
+
+
+cl.rt.models.single <- dataSingle %>%
+    select(starts_with("agr.rt.cl"), state, stateName, z.relig) %>%
+    mutate(Model="CLPM",
+           Type="Single")
+names(cl.rt.models.single) <- c("rt.cl", "ub", "lb", "stateId", "State", "Religiosity", "Model", "Type")
+## Load warnings
+load("results/clpm.single.warnings.RData")
+## Find any with problems
+problems <- grep("instabilities|positive\ definite|variances\ are\ negative", stateWarnings[["agr"]])
+cl.rt.models.single[!(cl.rt.models.single$stateId %in% problems), "select"] <- 1
+
+
+cl.rt.models.ri.single <- dataRiSingle %>%
+    select(starts_with("agr.rt.cl"), state, stateName, z.relig) %>%
+    mutate(Model="RI-CLPM",
+           Type="Single")
+names(cl.rt.models.ri.single) <- c("rt.cl", "ub", "lb", "stateId", "State", "Religiosity", "Model", "Type")
+## Load warnings
+load("results/riclpm.single.warnings.RData")
+## Find any with problems
+problems <- grep("instabilities|positive\ definite|variances\ are\ negative", stateWarnings[["agr"]])
+cl.rt.models.ri.single[!(cl.rt.models.ri.single$stateId %in% problems), "select"] <- 1
+
+
+allModels <- rbind(cl.rt.models.orig,
+                   cl.rt.models.ri,
+                   cl.rt.models.obs,
+                   cl.rt.models.ri.obs,
+                   cl.rt.models.single,
+                   cl.rt.models.ri.single)
+allModels$Model <- as.factor(allModels$Model)
+allModels$Type <- as.factor(allModels$Type)
+
+testPlot <- ggplot(data=subset(allModels[allModels$select==1,], !is.na(Model)),
+                   aes(x=State,
+                       y=rt.cl,
+                       ymin=lb,
+                       ymax=ub,
+                       color=Model,
+                       linetype=Type)) +
+    geom_point(position=position_dodge(width=0.5)) +
+    geom_errorbar(width=.05, position=position_dodge(width=0.5)) + coord_flip()
+testPlot
+    
+                       
+
 ################################################################################
 ## Functions
 ################################################################################
@@ -56,7 +205,7 @@ stateMeta <- function(trait, data, varNames, single=FALSE, moderator=FALSE) {
 ################################################################################
 
 data <- read_csv("results/clpm.latent.states.aggregated.estimates.csv")
-data <- left_join(data, religiosity[, c("state", "z.relig")], by = "state")
+data <- left_join(data, bulaList[, c("state", "stateName", "z.relig")], by = "state")
 
 stateMeta("agr", data, varNames)
 stateMeta("agr", data, varNames, moderator = TRUE)
@@ -79,7 +228,7 @@ stateMeta("opn", data, varNames, moderator = TRUE)
 ################################################################################
 
 dataRi <- read_csv("results/riclpm.latent.states.aggregated.estimates.csv")
-dataRi <- left_join(dataRi, religiosity[, c("state", "z.relig")], by = "state")
+dataRi <- left_join(dataRi, bulaList[, c("state", "stateName", "z.relig")], by = "state")
 
 stateMeta("agr", dataRi, varNames)
 stateMeta("agr", dataRi, varNames, moderator = TRUE)
@@ -103,7 +252,7 @@ stateMeta("opn", dataRi, varNames, moderator = TRUE)
 ################################################################################
 
 dataObs <- read_csv("results/clpm.observed.states.aggregated.estimates.csv")
-dataObs <- left_join(dataObs, religiosity[, c("state", "z.relig")], by = "state")
+dataObs <- left_join(dataObs, bulaList[, c("state", "stateName", "z.relig")], by = "state")
 
 stateMeta("agr", dataObs, varNames)
 stateMeta("agr", dataObs, varNames, moderator = TRUE)
@@ -126,7 +275,7 @@ stateMeta("opn", dataObs, varNames, moderator = TRUE)
 ################################################################################
 
 dataRiObs <- read_csv("results/riclpm.observed.states.aggregated.estimates.csv")
-dataRiObs <- left_join(dataRiObs, religiosity[, c("state", "z.relig")], by = "state")
+dataRiObs <- left_join(dataRiObs, bulaList[, c("state", "stateName", "z.relig")], by = "state")
 
 stateMeta("agr", dataRiObs, varNames)
 stateMeta("agr", dataRiObs, varNames, moderator = TRUE)
@@ -144,13 +293,19 @@ stateMeta("opn", dataRiObs, varNames)
 stateMeta("opn", dataRiObs, varNames, moderator = TRUE)
 
 
+################################################################################
+## CLPM: Single-Trait Models
+################################################################################
+
+dataSingle <- read_csv("results/clpm.states.single.estimates.csv")
+dataSingle <- left_join(dataSingle, bulaList[, c("state", "stateName", "z.relig")], by = "state")
 
 ################################################################################
 ## RICLPM: Single-Trait Models
 ################################################################################
 
 dataRiSingle <- read_csv("results/riclpm.states.single.estimates.csv")
-dataRiSingle <- left_join(dataRiSingle, religiosity[, c("state", "z.relig")], by = "state")
+dataRiSingle <- left_join(dataRiSingle, bulaList[, c("state", "stateName", "z.relig")], by = "state")
 
 stateMeta("agr", dataRiSingle, varNames, single = TRUE, moderator = FALSE)
 stateMeta("agr", dataRiSingle, varNames, single = TRUE, moderator = TRUE)
