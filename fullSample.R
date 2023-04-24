72248dfdbcce81d28aa859b674e5786ae50a71d0
rich <rlucas11@yahoo.com>
2023-04-24 17:24:44 -0400

    Figure for full-sample models

--- fullSample.R
+++ fullSample.R
@@ -637,5 +637,262 @@ single.riclpm.obs.results <- list(
 )
 save(single.riclpm.obs.results,
     file = paste0(location, "/single.riclpm.obs.results.RData")
-    )
+)
 
+
+################################################################################
+## Collect results for full sample models
+################################################################################
+
+## Load libraries (if not already done)
+library(tidyverse)
+library(ggplot2)
+
+## Setup labels
+traitLabels <- matrix(
+    c(
+        "agr", "a",
+        "cns", "c",
+        "ext", "e",
+        "neu", "n",
+        "opn", "o"
+    ),
+    nrow = 5, ncol = 2, byrow = TRUE
+)
+
+
+#### Functions to extract and summarize
+#### For models with all traits
+## Extract averages across multiple waves
+extractAvg <- function(results, trait) {
+    pLabel <- paste0(
+        "c_r",
+        trait
+    )
+    pLabel2 <- paste0(
+        "c_",
+        trait,
+        "r"
+    )
+    ## Religion predicted from trait
+    agg <- results %>%
+        filter(label == pLabel) %>%
+        select(est.std, ci.lower, ci.upper) %>%
+        summarise(
+            est = mean(est.std),
+            ci.lower = mean(ci.lower),
+            ci.upper = mean(ci.upper)
+        )
+    ## Trait predicted from religion
+    agg2 <- results %>%
+        filter(label == pLabel2) %>%
+        select(est.std, ci.lower, ci.upper) %>%
+        summarise(
+            est = mean(est.std),
+            ci.lower = mean(ci.lower),
+            ci.upper = mean(ci.upper)
+        )
+    return(c(agg, agg2))
+}
+
+## Extract results from full set
+extractParameterEstimates <- function(results,
+                                      model,
+                                      type,
+                                      variables) {
+    result <- data.frame(
+        est = numeric(),
+        ci.lower = numeric(),
+        ci.upper = numeric(),
+        est.1 = numeric(),
+        ci.lower.1 = numeric(),
+        ci.upper.1 = numeric(),
+        trait = character(),
+        model = character(),
+        type = character(),
+        variables = character()
+    )
+    for (i in 1:nrow(traitLabels)) {
+        trait <- traitLabels[i, 2]
+        result[i, ] <- c(
+            extractAvg(results, trait),
+            traitLabels[i,1],
+            model,
+            type,
+            variables
+        )
+    }
+    return(result)
+}
+
+#### Functions to extract results
+#### These are for single-trait models
+## Extract average values across waves
+extractAvgSingle <- function(results) {
+    ## Religion predicted from trait
+    agg <- results %>%
+        filter(label == "cl_t") %>%
+        select(est.std, ci.lower, ci.upper) %>%
+        summarise(
+            est = mean(est.std),
+            ci.lower = mean(ci.lower),
+            ci.upper = mean(ci.upper)
+        )
+    ## Trait predicted from religion
+    agg2 <- results %>%
+        filter(label == "cl_r") %>%
+        select(est.std, ci.lower, ci.upper) %>%
+        summarise(
+            est = mean(est.std),
+            ci.lower = mean(ci.lower),
+            ci.upper = mean(ci.upper)
+        )
+    return(c(agg, agg2))
+}
+
+## Extract estimates across all models
+extractParameterEstimatesSingle <- function(results,
+                                            model,
+                                            type,
+                                            variables) {
+    result <- data.frame(
+        est = numeric(),
+        ci.lower = numeric(),
+        ci.upper = numeric(),
+        est.1 = numeric(),
+        ci.lower.1 = numeric(),
+        ci.upper.1 = numeric(),
+        trait = character(),
+        model = character(),
+        type = character(),
+        variables = character()
+    )
+    for (i in 1:nrow(traitLabels)) {
+        trait <- traitLabels[i, 2]
+        result[i, ] <- c(
+            extractAvgSingle(results[[i]][[1]]),
+            traitLabels[i,1],
+            model,
+            type,
+            variables
+        )
+    }
+    return(result)
+}
+
+location <- "testResults"
+location <- "results"
+
+## Load results for each set of models
+load(paste0(location, "/clpm.latent.results.RData"))
+results.c.l.a <- clpm.latent.results[2][[1]]
+c.l.a <- extractParameterEstimates(
+    results.c.l.a,
+    "clpm",
+    "latent",
+    "all"
+)
+c.l.a
+
+load(paste0(location, "/riclpm.latent.results.RData"))
+results.r.l.a <- riclpm.latent.results[2][[1]]
+r.l.a <- extractParameterEstimates(
+    results.r.l.a,
+    "riclpm",
+    "latent",
+    "all"
+)
+r.l.a
+
+load(paste0(location, "/clpm.observed.results.RData"))
+results.c.o.a <- clpm.observed.results[2][[1]]
+c.o.a <- extractParameterEstimates(
+    results.c.o.a,
+    "clpm",
+    "observed",
+    "all"
+)
+c.o.a
+
+load(paste0(location, "/riclpm.observed.results.RData"))
+results.r.o.a <- riclpm.observed.results[2][[1]]
+r.o.a <- extractParameterEstimates(
+    results.r.o.a,
+    "riclpm",
+    "observed",
+    "all"
+)
+r.o.a
+
+load(paste0(location, "/single.clpm.latent.results.RData"))
+c.l.s <- extractParameterEstimatesSingle(
+    single.clpm.latent.results,
+    "clpm",
+    "latent",
+    "single"
+)
+c.l.s
+
+load(paste0(location, "/single.riclpm.latent.results.RData"))
+r.l.s <- extractParameterEstimatesSingle(
+    single.riclpm.latent.results,
+    "riclpm",
+    "latent",
+    "single"
+)
+r.l.s
+
+load(paste0(location, "/single.clpm.obs.results.RData"))
+c.o.s <- extractParameterEstimatesSingle(
+    single.clpm.obs.results,
+    "clpm",
+    "observed",
+    "single"
+)
+c.o.s
+
+load(paste0(location, "/single.riclpm.obs.results.RData"))
+r.o.s <- extractParameterEstimatesSingle(
+    single.riclpm.obs.results,
+    "riclpm",
+    "observed",
+    "single"
+)
+r.o.s
+
+
+
+## Collect data from all models
+plotData <- rbind(
+    c.l.a,
+    r.l.a,
+    c.o.a,
+    r.o.a,
+    c.l.s,
+    r.l.s,
+    c.o.s,
+    r.o.s
+)
+
+
+plotData %>%
+    ggplot(
+        aes(
+            x = trait,
+            y = est,
+            ymin = ci.lower,
+            ymax = ci.upper,
+            color = model,
+            linetype = type,
+            shape = variables
+        )
+    ) +
+    geom_point(position = position_dodge(width = 0.5), size=3) +
+        geom_errorbar(width = .05, position = position_dodge(width = 0.5)) +
+        coord_flip() +
+        theme_bw() +
+        scale_color_grey() +
+        theme(
+            panel.grid.major.y = element_blank(),
+            panel.grid.minor = element_blank()
+        )
